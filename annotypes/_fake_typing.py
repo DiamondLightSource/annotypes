b13ddab8adac697c6dda552ddb3249e87845e7f2
Tom Cobb <tom.cobb@diamond.ac.uk>
2017-12-19 15:49:24 +0000

    Delete more code

--- annotypes/_fake_typing.py
+++ annotypes/_fake_typing.py
@@ -23,107 +23,11 @@ class _TypingBase(object):
         pass
 
 
-def _get_type_vars(types, tvars):
-    for t in types:
-        if isinstance(t, TypingMeta) or isinstance(t, _TypingBase):
-            t._get_type_vars(tvars)
-
-
-def _type_vars(types):
-    tvars = []
-    _get_type_vars(types, tvars)
-    return tuple(tvars)
-
-
 class TypeVar(_TypingBase):
-    """Type variable.
-
-    Usage::
-
-      T = TypeVar('T')  # Can be anything
-      A = TypeVar('A', str, bytes)  # Must be str or bytes
-
-    Type variables exist primarily for the benefit of static type
-    checkers.  They serve as the parameters for generic types as well
-    as for generic function definitions.  See class Generic for more
-    information on generic types.  Generic functions work as follows:
-
-      def repeat(x: T, n: int) -> List[T]:
-          '''Return a list containing n references to x.'''
-          return [x]*n
-
-      def longest(x: A, y: A) -> A:
-          '''Return the longest of two strings.'''
-          return x if len(x) >= len(y) else y
-
-    The latter example's signature is essentially the overloading
-    of (str, str) -> str and (bytes, bytes) -> bytes.  Also note
-    that if the arguments are instances of some subclass of str,
-    the return type is still plain str.
-
-    At runtime, isinstance(x, T) and issubclass(C, T) will raise TypeError.
-
-    Type variables defined with covariant=True or contravariant=True
-    can be used do declare covariant or contravariant generic types.
-    See PEP 484 for more details. By default generic types are invariant
-    in all type variables.
-
-    Type variables can be introspected. e.g.:
-
-      T.__name__ == 'T'
-      T.__constraints__ == ()
-      T.__covariant__ == False
-      T.__contravariant__ = False
-      A.__constraints__ == (str, bytes)
-    """
-
     __metaclass__ = TypingMeta
-    __slots__ = ('__name__', '__bound__', '__constraints__',
-                 '__covariant__', '__contravariant__')
-
-    def __init__(self, name, *constraints, **kwargs):
-        super(TypeVar, self).__init__(name, *constraints, **kwargs)
-        bound = kwargs.get('bound', None)
-        covariant = kwargs.get('covariant', False)
-        contravariant = kwargs.get('contravariant', False)
-        self.__name__ = name
-        if covariant and contravariant:
-            raise ValueError("Bivariant types are not supported.")
-        self.__covariant__ = bool(covariant)
-        self.__contravariant__ = bool(contravariant)
-        if constraints and bound is not None:
-            raise TypeError("Constraints cannot be combined with bound=...")
-        if constraints and len(constraints) == 1:
-            raise TypeError("A single constraint is not allowed")
-        self.__constraints__ = tuple(constraints)
-        self.__bound__ = bound
-
-    def _get_type_vars(self, tvars):
-        if self not in tvars:
-            tvars.append(self)
-
-    def __instancecheck__(self, instance):
-        raise TypeError("Type variables cannot be used with isinstance().")
-
-    def __subclasscheck__(self, cls):
-        raise TypeError("Type variables cannot be used with issubclass().")
 
 
-# Some unconstrained type variables.  These are used by the container types.
-# (These are not for export.)
-T = TypeVar('T')  # Any type.
-T_co = TypeVar('T_co', covariant=True)  # Any type covariant containers.
-
-
-# Special typing constructs Union, Optional, Generic, Callable and Tuple
-# use three special attributes for internal bookkeeping of generic types:
-# * __parameters__ is a tuple of unique free type parameters of a generic
-#   type, for example, Dict[T, T].__parameters__ == (T,);
-# * __origin__ keeps a reference to a type that was subscripted,
-#   e.g., Union[T, int].__origin__ == Union;
-# * __args__ is a tuple of all arguments used in subscripting,
-#   e.g., Dict[T, int].__args__ == (T, int).
-
+T = TypeVar('T')
 
 _cleanups = []
 
@@ -154,12 +58,11 @@ def _tp_cache(func):
 
 class _Union(_TypingBase):
     __metaclass__ = TypingMeta
-    __slots__ = ('__parameters__', '__args__', '__origin__', '__tree_hash__')
+    __slots__ = ('__args__', '__origin__', '__tree_hash__')
 
     def __new__(cls, parameters=None, origin=None, *args, **kwds):
         self = super(_Union, cls).__new__(cls, parameters, origin, *args, **kwds)
         if origin is None:
-            self.__parameters__ = None
             self.__args__ = None
             self.__origin__ = None
             self.__tree_hash__ = hash(frozenset(('Union',)))
@@ -170,14 +73,9 @@ class _Union(_TypingBase):
             # It's not a union if there's only one type left.
             if len(parameters) == 1:
                 return parameters[0]
-        self.__parameters__ = _type_vars(parameters)
         self.__args__ = parameters
         self.__origin__ = origin
         return self
-
-    def _get_type_vars(self, tvars):
-        if self.__origin__ and self.__parameters__:
-            _get_type_vars(self.__parameters__, tvars)
 
     @_tp_cache
     def __getitem__(self, parameters):
@@ -277,33 +175,11 @@ class GenericMeta(TypingMeta, abc.ABCMeta):
     """
 
     def __new__(cls, name, bases, namespace,
-                tvars=None, args=None, origin=None, extra=None, orig_bases=None):
+                args=None, origin=None, extra=None, orig_bases=None):
         """Create a new generic class. GenericMeta.__new__ accepts
         keyword arguments that are used for internal bookkeeping, therefore
         an override should pass unused keyword arguments to super().
         """
-        if tvars is not None:
-            # Called from __getitem__() below.
-            assert origin is not None
-            assert all(isinstance(t, TypeVar) for t in tvars), tvars
-        else:
-            # Called from class statement.
-            assert tvars is None, tvars
-            assert args is None, args
-            assert origin is None, origin
-
-            # Get the full set of tvars from the bases.
-            tvars = _type_vars(bases)
-            # Look for Generic[T1, ..., Tn].
-            # If found, tvars must be a subset of it.
-            # If not found, tvars is it.
-            # Also check for and reject plain Generic,
-            # and reject multiple Generic[...].
-            for base in bases:
-                if (isinstance(base, GenericMeta) and
-                        base.__origin__ is Generic):
-                    tvars = base.__parameters__
-
         initial_bases = bases
         if extra is None:
             extra = namespace.get('__extra__')
@@ -317,7 +193,6 @@ class GenericMeta(TypingMeta, abc.ABCMeta):
         namespace.update({'__origin__': origin, '__extra__': extra})
         self = super(GenericMeta, cls).__new__(cls, name, bases, namespace)
 
-        self.__parameters__ = tvars
         self.__args__ = args
         # Speed hack (https://github.com/python/typing/issues/196).
         self.__next_in_mro__ = _next_in_mro(self)
@@ -378,28 +253,15 @@ class GenericMeta(TypingMeta, abc.ABCMeta):
             else:
                 self._abc_generic_negative_cache_version = value
 
-    def _get_type_vars(self, tvars):
-        if self.__origin__ and self.__parameters__:
-            _get_type_vars(self.__parameters__, tvars)
-
     @_tp_cache
     def __getitem__(self, params):
         if not isinstance(params, tuple):
             params = (params,)
-        if self is Generic:
-            tvars = params
-            args = params
-        else:
-            # Subscripting a regular Generic subclass.
-            tvars = _type_vars(params)
-            args = params
-
         prepend = (self,) if self.__origin__ is None else ()
         return self.__class__(self.__name__,
                               prepend + self.__bases__,
                               dict(self.__dict__),
-                              tvars=tvars,
-                              args=args,
+                              args=params,
                               origin=self,
                               extra=self.__extra__,
                               orig_bases=self.__orig_bases__)
@@ -462,17 +324,17 @@ def overload(func):
     return _overload_dummy
 
 
-class Iterable(Generic[T_co]):  # type: ignore
+class Iterable(Generic[T]):  # type: ignore
     __slots__ = ()
     __extra__ = collections_abc.Iterable
 
 
-class Container(Generic[T_co]):  # type: ignore
+class Container(Generic[T]):  # type: ignore
     __slots__ = ()
     __extra__ = collections_abc.Container
 
 
-class Sequence(collections_abc.Sized, Iterable[T_co], Container[T_co]):  # type: ignore
+class Sequence(collections_abc.Sized, Iterable[T], Container[T]):  # type: ignore
     __slots__ = ()
     __extra__ = collections_abc.Sequence
 
