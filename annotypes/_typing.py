49af5f023bf7deaced0a770cb6440cec4c4836eb
Tom Cobb <tom.cobb@diamond.ac.uk>
2017-12-19 15:32:19 +0000

    Make it clear which versions of python fake typing should be used on

--- annotypes/_typing.py
+++ annotypes/_typing.py
@@ -1,8 +1,15 @@
-try:
+import sys
+
+if sys.version_info < (3, 0):
+    try:
+        from typing import (
+            TYPE_CHECKING, TypeVar, Sequence, Union, Optional, Generic, overload
+        )
+    except ImportError:
+        from ._fake_typing import (
+            TYPE_CHECKING, TypeVar, Sequence, Union, Optional, Generic, overload
+        )
+else:
     from typing import (
-        TYPE_CHECKING, TypeVar, Sequence, Union, Optional, Generic, overload
-    )
-except ImportError:
-    from ._fake_typing import (  # type: ignore
         TYPE_CHECKING, TypeVar, Sequence, Union, Optional, Generic, overload
     )
