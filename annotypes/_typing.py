6c5bc7ba177012bef11c79baa917803a8058dd40
Giles Knap <giles.knap@diamond.ac.uk>
2019-07-02 11:15:59 +0100

    complete fake typing removal

--- annotypes/_typing.py
+++ annotypes/_typing.py
@@ -1,22 +1,13 @@
 import sys
 
-try:
-    from typing import (
-        TYPE_CHECKING, TypeVar, Sequence, Union, Optional, Generic,
-        overload, Mapping, Any
-    )
-    if sys.version_info >= (3, 7):
-        from abc import ABCMeta as GenericMeta
-        from collections.abc import Mapping as MappingOrigin
-        NEW_TYPING = True
-    else:
-        from typing import GenericMeta, Mapping as MappingOrigin
-        NEW_TYPING = False
-except ImportError:
-    if sys.version_info < (3, 0):
-        from ._fake_typing import (
-            TYPE_CHECKING, TypeVar, Sequence, Union, Optional, Generic,
-            overload, Mapping, Any, GenericMeta
-        )
-    else:
-        raise
+from typing import (
+    TYPE_CHECKING, TypeVar, Sequence, Union, Optional, Generic,
+    overload, Mapping, Any
+)
+if sys.version_info >= (3, 7):
+    from abc import ABCMeta as GenericMeta
+    from collections.abc import Mapping as MappingOrigin
+    NEW_TYPING = True
+else:
+    from typing import GenericMeta, Mapping as MappingOrigin
+    NEW_TYPING = False
